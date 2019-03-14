d53aa1d550adba22fa841032339f5a44d56afc06
juliastetskaya <juliastetskaya@gmail.com>
2019-03-14 09:31:31 +0300

    decision

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  // Your code goes here!
+  // Return an object containing the minimum number of coins needed to make change
+  if (currency < 1) {
+    return {};
+  }
+  if (currency > 10000) {
+    return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+  }
+  const dict = new Map([[50, 'H'], [25, 'Q'], [10, 'D'], [5, 'N'], [1, 'P']]);
+  const arr = [50, 25, 10, 5, 1];
+  return arr.reduce((acc, item) => {
+    const count = Math.floor(acc[0] / item);
+    return (count === 0 ? acc : [acc[0] - count * item, { ...acc[1], [dict.get(item)]: count }]);
+  }, [currency, {}])[1];
+  
 }
